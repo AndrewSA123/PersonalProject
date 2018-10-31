23175cee0ae2010aea36132049b42c1e265d8773
AndrewSA123 <andrewknott_123@hotmail.co.uk>
2018-10-31 11:01:12 +0000

    Adding movie persistence

--- src/main/java/com/qa/persistence/domain/Movie.java
+++ src/main/java/com/qa/persistence/domain/Movie.java
@@ -1,5 +1,49 @@
 package com.qa.persistence.domain;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Movie {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long mid;
+	private String title;
+	private long fk_did;
+	private String genre;
+
+	public Movie() {
+
+	}
+
+	public Movie(String title, String genre) {
+		this.title = title;
+		this.genre = genre;
+	}
+
+	public Long getMid() {
+		return this.mid;
+	}
+
+	public void setMid(Long id) {
+		this.mid = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return this.title;
+	}
+
+	public String getGenre() {
+		return this.genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
 
 }
